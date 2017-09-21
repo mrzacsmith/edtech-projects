0ab650e126f5019baecaaa987926d0380d3f144b
Zac Smith <zrs3141592@gmail.com>
2017-09-21 14:24:51 -0600

    Add counter high-low game

--- high-low-game/high-low.rb
+++ high-low-game/high-low.rb
@@ -2,16 +2,20 @@ def game
   ran_num = rand(25)
   puts "Guess a number between 0 and 24:"
 
+  @counter = 1
   loop do
     user_answer = gets.chomp.to_i
 
     if user_answer == ran_num
       puts "You win! The correct number is #{ran_num}"
+      puts "#{@counter}"
       break
     elsif user_answer > ran_num
       puts "You guessed to high! Guess a lower number!"
+      @counter += 1
     elsif user_answer < ran_num
       puts "You guessed to low! Guess a higher number!"
+      @counter += 1
     end
   end
 end
