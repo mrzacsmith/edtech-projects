e1107a1d2682b899949834321fdc422bdedafb40
Zac Smith <zrs3141592@gmail.com>
2017-09-21 18:40:52 -0600

    Reverse the string using .split.reverse.join(' ')

--- reversing-words/reversing-words.rb
+++ reversing-words/reversing-words.rb
@@ -1,5 +1,8 @@
 require 'rspec'
 
+def sentence_reverser str
+  str.split.reverse.join(' ')
+end
 
 describe 'Sentence reversing' do
   it 'reverses a string' do
@@ -7,3 +10,5 @@ describe 'Sentence reversing' do
     expect(sentence_reverser(text_sentence)).to eq("I am backwards")
   end
 end
+
+puts sentence_reverser 'These are happy damn days'
